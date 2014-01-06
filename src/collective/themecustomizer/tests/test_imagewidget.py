af53a51140eab4bf2ce73cdf92f6576b5d55534b
Marcos F. Romero <marcosfromero@gmail.com>
2014-01-06 18:13:52 -0300

    Fixed imagewidget test

--- src/collective/themecustomizer/tests/test_imagewidget.py
+++ src/collective/themecustomizer/tests/test_imagewidget.py
@@ -12,10 +12,10 @@ RENDERED = u'\n  <input type="hidden" value="" name="field.foo.used" id="field.f
 class="noborder" name="field.foo.action" onclick="document.getElementById(\'field.foo\').disabled=true" \
 id="field.foo.nochange" />\n    <label for="field.foo.nochange">Keep existing image</label>\n    \
 <br />\n    <input type="radio" value="remove" class="noborder" name="field.foo.action" \
-onclick="document.getElementById(\'field.foo\').disabled=true" id="field.foo.delete" />   \n    \
+onclick="document.getElementById(\'field.foo\').disabled=true" id="field.foo.delete" />\n    \
 <label for="field.foo.replace">Remove existing image</label>\n    <br />\n    \
 <input type="radio" value="replace" class="noborder" name="field.foo.action" \
-onclick="document.getElementById(\'field.foo\').disabled=false" id="field.foo.replace" />   \n    \
+onclick="document.getElementById(\'field.foo\').disabled=false" id="field.foo.replace" />\n    \
 <label for="field.foo.replace">Replace with new image</label>\n  </div>\n  \
 <div style="padding-left: 1.5em; padding-top: 0.5em;">\n    \
 <input type="file" maxlength="True" class="" size="30" name="field.foo" id="field.foo" />\n    \
@@ -41,4 +41,5 @@ class ImageWidgetTestCase(unittest.TestCase):
         widget = ImageWidget(field, request)
 
         self.assertEqual(widget.getInputValue(), 'remove')
+        import pdb; pdb.set_trace()
         self.assertEqual(widget(), RENDERED)
